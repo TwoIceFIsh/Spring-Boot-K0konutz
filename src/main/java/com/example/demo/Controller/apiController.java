4e8e5d66e86a5ba1ebc360a7f763e21a931f3aa1
Aiden Lee <team.k0konutz@gmail.com>
2022-05-19 22:27:55 +0900

    Version 20220519v
    메일시스템 추가기능 생성 중

--- src/main/java/com/example/demo/Controller/apiController.java
+++ src/main/java/com/example/demo/Controller/apiController.java
@@ -1,6 +1,7 @@
 package com.example.demo.Controller;
 
 
+import com.example.demo.DTO.mailing.mailingDTO;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class apiController {
 
     @PostMapping("/emailCheck")
-    public ResponseEntity mailContll(@RequestBody String data) {
-        System.out.println(data);
-        log.debug("z");
+    public ResponseEntity mailContll(@RequestBody mailingDTO address) {
+
+        log.info(address.getAddress());
         return ResponseEntity.status(HttpStatus.OK).body('1');
 
     }
--- src/main/resources/static/js/mailing.js
+++ src/main/resources/static/js/mailing.js
@@ -3,15 +3,14 @@ function action() {
     const email = $('#email').val();
     alert(email);
 
-    let formData = new FormData();
-    formData.append('email', email);
+    formData = {'email': email};
+
 
     fetch('/emailCheck', {
         method: 'post',
         body: JSON.stringify(formData)
     }).then(res => {
-
-        alert(Promise.resolve(res));
+        alert(JSON.stringify(formData));
         if (res === 1)
             alert('구독신청이 완료되었습니다.');
         else
