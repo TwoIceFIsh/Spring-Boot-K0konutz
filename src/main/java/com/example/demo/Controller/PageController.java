459eded0cd56f4912b664f6c67e366a354d39b1c
Aiden Lee <team.k0konutz@gmail.com>
2022-05-23 21:53:11 +0900

    Version 20220523v
    소개페이지 작성

--- src/main/java/com/example/demo/Controller/PageController.java
+++ src/main/java/com/example/demo/Controller/PageController.java
@@ -33,8 +33,8 @@ public class PageController {
     }
 
     @GetMapping("/test")
-    public String goKokonutz() {
-        return "kokonutz/main";
+    public String mm() {
+        return "test";
     }
 
     @GetMapping("/kokonutz")
