1b6e0ae937bfc06c28df4ca1272aa8267f422a26
Aiden Lee <team.k0konutz@gmail.com>
2022-06-17 21:40:51 +0900

    Version 20220617v
    메일 서비스 정상 오픈

--- src/main/resources/static/modules/boho_mail.py
+++ src/main/resources/static/modules/boho_mail.py
@@ -1,146 +1,29 @@
-import smtplib
-import time
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import formataddr
 import datetime
+import time
+from mail_func import *
 
-import psycopg2
-import requests
-from bs4 import BeautifulSoup
+while True:
+    print('Scanning : ' + str(datetime.datetime.now()))
 
+    # 신규 게시물 확인
+    article_list = get_text_list(file_name='./article_lists.txt')
+    new_article_list = get_data(url='https://www.boho.or.kr/data/secNoticeList.do')
+    newest_article = what_is_new_article(article_list=article_list, new_article_list=new_article_list)
 
-def sendMail(output,  email):
-    print(
-        "############################################## 메일 보내기 ##########################################################")
+    # 신규 게시글 파일 작성성
+    file_set_article(file_name='./article_lists.txt', articles=new_article_list)
 
-    ################ 이메일 카운트 증가 ##################
+    # 이메일 보내기
+    mail_list = get_text_list(file_name='./mail_list.txt')
+    article_text = article_to_html(newest_article=newest_article)
 
-    ###############################################################
-    from_addr = formataddr(('SOCH', 'bh.lee@email.com'))
+    if len(newest_article) > 0:
+        for to in mail_list:
+            sendMail(article=article_text, new_num=len(newest_article), to_ad=to)
 
-    # 받는사람
-    to_addr = formataddr(('보안담당자', email))
-
-    session = None
-    try:
-        # SMTP 세션 생성
-        session = smtplib.SMTP('smtp.gmail.com', 587)
-        session.set_debuglevel(True)
-
-        # SMTP 계정 인증 설정
-        session.ehlo()
-        session.starttls()
-        session.login('igloosoil@gmail.com', 'lougwydyuijffjcd')
-
-        # 메일 콘텐츠 설정
-        message = MIMEMultipart("mixed")
-
-        # 메일 송/수신 옵션 설정
-        message.set_charset('utf-8')
-        message['From'] = from_addr
-        message['To'] = to_addr
-        message['Subject'] = "[보안관제] 보호나라 새로운 게시물알림"
-
-        # 메일 콘텐츠 - 내용
-        body = output + "<br><br><br>문의 : Aiden Lee(이병호)<br>솔루션 주소 : https://www.kokonut.today/mail "
-
-        bodyPart = MIMEText(body, 'html', 'utf-8')
-        message.attach(bodyPart)
-
-        # 메일 콘텐츠 - 첨부파일
-
-        # 메일 발송
-        session.sendmail(from_addr, to_addr, message.as_string())
-
-
-    except Exception as e:
-        print(e)
-    finally:
-        if session is not None:
-            session.quit()
-
-    #################################################################################################################
-
-
-# 15분마다 크롤링
-
-while (True):
-    now = datetime.datetime.now()
-    print('수집중 : ' + str(now))
-    url = 'https://www.boho.or.kr/data/secNoticeList.do'
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        html = response.text.strip()
-        soup = BeautifulSoup(html, 'html.parser')
-
-        b_id = soup.select('#contentDiv > table > tbody > tr.first > td.colTit > a')[0]['href'].split('?')[1].replace(
-            'bulletin_writing_sequence=', '')
-        b_no = soup.select('#contentDiv > table > tbody > tr.first > td:nth-child(1)')[0].text.strip()
-        b_title = soup.select('#contentDiv > table > tbody > tr.first > td.colTit > a')[0].text.strip()
-        b_date = soup.select('#contentDiv > table > tbody > tr.first > td:nth-child(5)')[0].text.strip()
-        b_url = 'https://www.boho.or.kr/' + soup.select('#contentDiv > table > tbody > tr.first > td.colTit > a')[0][
-            'href']
-        output = ''
-        db = psycopg2.connect(host='localhost', dbname='postgres', user='postgres', password='Dlqudgh1!', port=5432)
-        cursor = db.cursor()
-
-        print(b_id + b_no)
-
-        try:
-            cursor.execute("INSERT INTO b_table (b_id,b_no,b_title,b_date,b_url) VALUES (%s,%s,%s,%s,%s)",
-                           (b_id, b_no, b_title, b_date, b_url))
-            print(b_id + ' ' + b_no + ' ' + b_title + ' ' + b_date + ' ' + b_url)
-            output = output + b_id + ' ' + b_no + ' ' + b_title + ' ' + b_date + ' ' + b_url + "<br>"
-        except psycopg2.errors.UniqueViolation:
-            print()
-
-        cursor.close()
-        db.commit()
-        db.close()
-
-        if '[공지]' in b_no:
-            d_no = 404
-
-        for i in range(2, 10):
-
-            print('next Logic' + str(i))
-
-            b_id = \
-            soup.select('#contentDiv > table > tbody > tr:nth-child(' + str(i) + ') > td.colTit > a')[0]['href'].split(
-                '?')[1].replace(
-                'bulletin_writing_sequence=', '')
-            b_no = soup.select('#contentDiv > table > tbody > tr:nth-child(' + str(i) + ') > td:nth-child(1)')[
-                0].text.strip()
-            b_title = soup.select('#contentDiv > table > tbody > tr:nth-child(' + str(i) + ') > td.colTit > a')[
-                0].text.strip()
-            b_date = soup.select('#contentDiv > table > tbody > tr:nth-child(' + str(i) + ') > td:nth-child(5)')[
-                0].text.strip()
-            b_url = 'https://www.boho.or.kr/' + \
-                    soup.select('#contentDiv > table > tbody > tr:nth-child(' + str(i) + ') > td.colTit > a')[0]['href']
-
-            db = psycopg2.connect(host='localhost', dbname='postgres', user='postgres', password='Dlqudgh1!', port=5432)
-            cursor = db.cursor()
-
-            print(b_id + b_no)
-            try:
-                cursor.execute("INSERT INTO b_table (b_id,b_no,b_title,b_date,b_url) VALUES (%s,%s,%s,%s,%s)",
-                               (b_id, b_no, b_title, b_date, b_url))
-                print(b_id + ' ' + b_no + ' ' + b_title + ' ' + b_date + ' ' + b_url)
-                output = output + b_id + ' ' + b_no + ' ' + b_title + ' ' + b_date + ' ' + b_url + "<br>"
-            except psycopg2.errors.UniqueViolation:
-                print()
-
-            cursor.close()
-            db.commit()
-            db.close()
-        
-        if output != '':
-            f = open('./mail_list.txt', 'r')
-            for email_address in f.readlines():
-                print(email_address)
-                sendMail(output,  email_address.strip().replace('\n',''))
-
-    print('end')
+        now = datetime.datetime.now()
+        print('발송 : '+ str(now) + ' : ' + ','.join(mail_list))
+        for i in enumerate(newest_article, start=1):
+            print(i)
+    print('------------ Sleep 15 min -----------')
     time.sleep(900)
