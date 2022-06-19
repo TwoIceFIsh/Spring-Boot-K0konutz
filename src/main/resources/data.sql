ca84b65de388ecbc4660129b9c2958f19c210601
Aiden Lee <team.k0konutz@gmail.com>
2022-06-19 11:42:11 +0900

    20220618v
    1. mailing 개편작업

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO ARTICLE_ENTITY(id, title, url, date)
+VALUES (1, '샘플데이터', 'https://www.naver.com', '2022.05.19');
+INSERT INTO ARTICLE_ENTITY(id, title, url, date)
+VALUES (2, '샘플데이터', 'https://www.kokonut.today', '2022.04.19');
+INSERT INTO ARTICLE_ENTITY(id, title, url, date)
+VALUES (3, '샘플데이터', 'https://www.daum.net', '2022.06.22');
+INSERT INTO ARTICLE_ENTITY(id, title, url, date)
+VALUES (4, '샘플데이터', 'https://www.naver.com', '2022.03.17');
